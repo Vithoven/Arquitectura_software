e9c776ed32c238a92398a43fb1501931372e06c0
Vito Bozzano <vi.bozzano@duocuc.cl>
2024-11-17 12:14:50 -0300

    arreglos de rutas e inicializacion

--- gestor_pagos/app/__init__.py
+++ gestor_pagos/app/__init__.py
@@ -9,11 +9,11 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
     db.init_app(app)
     migrate.init_app(app, db)
-    
+
     from app.routes import main_bp
     app.register_blueprint(main_bp)
-    
+
     return app
